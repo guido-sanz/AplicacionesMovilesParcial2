269d87b95c09de257ec629be2e63e9f0c14d64aa
Dieego12 <dcostantino23@gmail.com>
2025-06-05 20:48:55 -0300

    Implementacion del GPS
    Se agrega las dependencias necesarias para la location y se setea todo para la implemtacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation(libs.play.services.location.license)
+    implementation(libs.accompanist.permissions)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
